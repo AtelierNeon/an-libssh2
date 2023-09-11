f9ceed74ae2886b8715d1f5c04e5253fcc486d79
Kene Lin <kenelin@gmail.com>
2023-09-11 13:10:07 +0800

    Add basic build script

---
+++ script/build-windows-preset.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+setlocal
+echo [Windows] Applying preset options ...
+set MY_PROJECT_LIBSSH2_WITH_DISABLED_TEST_APPS=OFF
+set MY_PROJECT_LIBSSH2_WITH_ZLIB=ON
+set MY_PROJECT_OPENSSL_WITH_DEPRECATED_CIPHERS=ON
+set MY_PROJECT_OPENSSL_WITH_DISABLED_APPS=ON
+set MY_PROJECT_OPENSSL_WITH_ZLIB=ON
+set MY_PROJECT_ZLIB_WITH_DISABLED_TEST_APPS=ON
+echo [Windows] Applying default options ... DONE
+call %~dp0\build-windows.cmd
+endlocal
